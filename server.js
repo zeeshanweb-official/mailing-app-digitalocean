088fd411802e46a5d8b1188992aba4b7db133dca
zeeshan <zeeshanweb.official@gmail.com>
2022-02-22 11:07:17 +0500

    latest working code

--- server.js
+++ server.js
@@ -15,29 +15,36 @@ app.get("/", function (req, res) {
   res.send(JSON.stringify({ message: "email sending server is working fine url is /send-email" }));
 });
 
+const transporter = nodeMailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "milhas.jean@gmail.com",
+    pass: "B023991wa",
+  },
+});
+
+const generateHTML= (body)=>{
+  let mainString =  ``
+  Object.keys(body).forEach((key)=>{
+    mainString += `<p>${key.includes("_")?key.split("_").join(" "):key} : ${body[key]}</p>`
+  })
+  return mainString
+}
+
+
+
+
 app.post("/send-email", function (req, res) {
-  let transporter = nodeMailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "milhas.jean@gmail.com",
-      pass: "B023991w",
-    },
-  });
-  const generateHTML= ()=>{
-    let mainString =  ``
-    Object.keys(req.body).forEach((key)=>{
-      mainString += `<p>${key.includes("_")?key.split("_").join(" "):key} : ${req.body[key]}</p>`
-    })
-    return mainString
-  }
-  let mailOptions = {
+
+  const mailOptions = {
     from: '"Krunal Lathiya"', // sender address
     to: "Warren.kiriakou@gmail.com", // list of receivers
     subject: `${req.body.cardholder_name} requested a new refund`, // Subject line
-    html: generateHTML(), // html body
+    html: generateHTML(req.body), // html body
   };
+  
   try {
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
